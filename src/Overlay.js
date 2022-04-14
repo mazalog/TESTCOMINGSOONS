87566ffb7d186395201311728f51b7c7a8d1a5a3
Optimos/ Andres Maza <66882970+opandre-123@users.noreply.github.com>
2022-04-14 00:38:33 -0400

    Update Overlay.js

--- src/Overlay.js
+++ src/Overlay.js
@@ -5,7 +5,7 @@ export default function Overlay({ ready, clicked, setClicked }) {
   return (
     <>
       <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${clicked && 'clicked'}`}>
-        <div onClick={() => ready && setClicked(true)}>{!ready ? 'loading' : 'click to continue'}</div>
+        <div onClick={() => ready && setClicked(true)}>{!ready ? 'loading' : 'Conectar a la emoción'}</div>
       </div>
 
     </>
