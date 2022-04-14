fea4ac8d13327b6724ef69a95deab7683965101a
Optimos/ Andres Maza <66882970+opandre-123@users.noreply.github.com>
2022-04-14 00:32:33 -0400

    Update Overlay.js

--- src/Overlay.js
+++ src/Overlay.js
@@ -7,12 +7,7 @@ export default function Overlay({ ready, clicked, setClicked }) {
       <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${clicked && 'clicked'}`}>
         <div onClick={() => ready && setClicked(true)}>{!ready ? 'loading' : 'click to continue'}</div>
       </div>
-      <Footer
-        date="28. January"
-        year="2021"
-        link1={<a href="https://github.com/pmndrs/drei">pmndrs/drei</a>}
-        link2={<a href="https://codesandbox.io/s/drei-reflector-bfplr">s/drei-reflector-bfplr</a>}
-      />
+
     </>
   )
 }
